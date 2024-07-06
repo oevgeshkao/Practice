b33af8950921a2b800d2aa5fc4930865de221c8a
e.s.belova <e.s.belova02.eb@gmail.com>
2024-07-06 18:37:27 +0300

    Задание 1

---
+++ src/main/java/base/TaskFirst.java
@@ -0,0 +1,9 @@
+package base;
+
+public class TaskFirst {
+    public static void main(String[] args) {
+        for (int number = 7; number < 100; number += 7) {
+            System.out.print(number+" ");
+        }
+    }
+}
