53de2256e0b7f577401b8d6af8f70e74783e4e3e
e.s.belova <e.s.belova02.eb@gmail.com>
2024-07-06 19:07:34 +0300

    Проверка

--- src/main/java/base/TaskFirst.java
+++ src/main/java/base/TaskFirst.java
@@ -4,6 +4,7 @@ public class TaskFirst {
     public static void main(String[] args) {
         for (int number = 7; number < 100; number += 7) {
             System.out.print(number+" ");
+            aaaaaaaaaaa
         }
     }
 }
