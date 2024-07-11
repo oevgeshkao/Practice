d93d9ef8f78c88c67e0b93e661111f46eff66c15
e.s.belova <e.s.belova02.eb@gmail.com>
2024-07-11 15:55:56 +0300

    Слияние заданий в 1 класс

--- src/main/java/base/TaskFirst.java
+++
@@ -1,9 +0,0 @@
-package base;
-
-public class TaskFirst {
-    public static void main(String[] args) {
-        for (int number = 7; number < 100; number += 7) {
-            System.out.print(number+" ");
-        }
-    }
-}
